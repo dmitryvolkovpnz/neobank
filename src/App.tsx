d78099b609eb1085d678055505cff6a8dd469f14
DmitryHome <dvolkov0422@gmail.com>
2024-11-26 23:08:02 +0300

    create mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import Header from "./ components/Header";
 import Main from "./ components/Main";
 import Footer from "./ components/Footer";
 import "./styles/style.scss";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header/>
-      <Main/>
+        <Routes>
+            <Route path="/" Component={Main}/>
+        </Routes>
       <Footer/>
     </div>
   );
