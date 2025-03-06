1767dafbaf80e743d63109f84806e86cd8e5ca17
dmitryvolkovpnz <dvolkov0422@gmail.com>
2025-03-06 05:50:54 +0300

    add 404 error page

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Home from './page/Home';
 import HomeLayout from './layouts/home-layouts';
 import Loanpage from './page/Loanpage';
-import CrediOffers from "./components/loanpage/CreditOffers/CrediOffers";
+import ErrorPage from "./page/404";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route element={<HomeLayout/>}>
-              <Route path="/" Component={Home}/>
-              <Route path="/loan-page" Component={Loanpage}/>
-              <Route path="/new" element={<CrediOffers/>} />
+                <Route path="/" Component={Home}/>
+                <Route path="/loan-page" Component={Loanpage}/>
+                <Route path="*" Component={ErrorPage}/>
             </Route>  
         </Routes>
     </BrowserRouter>
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,11 +1,13 @@
 import "./button.scss";
+import React from "react";
+import {string} from "yup";
 
 type ButtonProps = {
-    children: React.ReactNode;
+    children: React.ReactNode,
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({children}) => {
     return <button className="button" type="submit">{children}</button>;
-  };
+};
 
 export default Button;
